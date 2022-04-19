bbe2fcf089de68caadc52db42e91c2f4f61386c8
Lala246_sh <yusufshukurah59@gmail.com>
2022-04-19 08:51:37 +0000

    print numbers

--- print_nums.c
+++ print_nums.c
@@ -18,3 +18,63 @@ int print_int(va_list l, flags_t *f)
 	print_number(n);
 	return (res);
 }
+
+/**
+ * print_unsigned - prints undigned int.
+ * @l: argument
+ * @f: pointer
+ * Return: number of char printed
+ */
+int print_unsigned(va_list l, flags_t *f)
+{
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
+
+	(void)f;
+	return (_puts(str));
+}
+
+/**
+ * print_number - function that loops through.
+ * @n: int to be printed.
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	} else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n1 % 10) + '0');
+}
+
+/**
+ * count_digit - returns number of digits
+ * @i: integer to evaluate
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
+}
